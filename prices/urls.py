88a1653c30094d21c847b76f5a42a2f2b0134fd1
ClaudiaLie <claudiss.lai@gmail.com>
2021-09-20 00:34:31 +0000

    Outlined a Calendar for the Gym Classes

--- prices/urls.py
+++ prices/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.prices, name='prices'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
--- prices/views.py
+++ prices/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def prices(request):
     """ Return the prices page """
     return render(request, 'prices/prices.html')
+
+
+def view_bag(request):
+    """ Render the bag contents page """
+
+    return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add to the shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+
+    bag = request.session.get('bag', {})
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
